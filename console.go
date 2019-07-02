fe1f1a886627f348795660831ef7325e89de8635
wzhu <wang-zhu@live.com>
2019-07-02 16:36:29 +0800

    Initial commit

---
+++ console.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+)
+import _ "console/router"
+
+func main() {
+	beego.Run()
+}
---
+++ controller/file.go
@@ -0,0 +1,44 @@
+package controller
+
+import (
+	"console/service"
+	"fmt"
+	"github.com/astaxie/beego"
+	"io"
+	"log"
+	"strconv"
+)
+
+type FileController struct {
+	beego.Controller
+}
+
+func (it *FileController) Upload() {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Printf(" 错误 %s\r\n", e)
+		}
+	}()
+	file, header, e := it.GetFile("image_file")
+	if e != nil {
+		panic(e)
+	}
+	i, e := strconv.Atoi(it.Ctx.Request.Form.Get("type"))
+	info := &service.ImageSimpleBuildInfo{
+		Name:    it.Ctx.Request.Form.Get("image_name"),
+		Version: it.Ctx.Request.Form.Get("image_version"),
+		Prefix:  it.Ctx.Request.Form.Get("image_prefix"),
+		Type:    i,
+	}
+	defer file.Close()
+	r := file.(io.Reader)
+	flag := make(chan struct{})
+	defer close(flag)
+	go service.DockerImageBuild(&r, info, flag)
+	log.Println(header.Filename)
+	<-flag
+	e = it.Ctx.Output.Body([]byte(`{"msg":"ok"}`))
+	if e != nil {
+		panic(e)
+	}
+}
