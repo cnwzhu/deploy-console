adc8cf4289d60a65edf19ed80406bb8c4b265487
wangzhu <wangzhu@originaltek.com>
2019-07-03 10:42:44 +0800

    支持自定义Dockerfile

--- service/docker.go
+++ service/docker.go
@@ -32,7 +32,7 @@ type ImageSimpleBuildInfo struct {
 }
 
 type DockerfileInfo struct {
-	BaseImage, UserName, Email, Location, Port string
+	BaseImage, UserName, Email, Location, Port, Cmd string
 }
 
 var c *client.Client
@@ -58,15 +58,22 @@ func DockerImageBuild(file *io.Reader, simpleBuildInfo *ImageSimpleBuildInfo, ch
 			fmt.Printf("docker build 错误 %s\r\n", e)
 		}
 	}()
-	reader := preBuild(file, header, simpleBuildInfo.Type)
-	doBuild(reader, simpleBuildInfo, ch)
+	if simpleBuildInfo.Type == SELF {
+		doBuild(file, simpleBuildInfo, ch)
+	} else {
+		reader := preBuild(file, header, simpleBuildInfo.Type)
+		doBuild(reader, simpleBuildInfo, ch)
+	}
 }
 
 func preBuild(file *io.Reader, header *multipart.FileHeader, ty int) *io.Reader {
 	source := rand.NewSource(time.Now().UnixNano())
 	rd := rand.New(source)
 	tempDir := os.TempDir() + "\\docker" + strconv.Itoa(rd.Int())
-	os.Mkdir(tempDir, os.ModePerm)
+	err := os.Mkdir(tempDir, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 	e := os.Chdir(tempDir)
 	if e != nil {
 		panic(e)
@@ -78,15 +85,21 @@ func preBuild(file *io.Reader, header *multipart.FileHeader, ty int) *io.Reader 
 		}
 	} else {
 		target, e := os.Create(header.Filename)
-		var t io.Writer = target
-		io.Copy(t, *file)
 		if e != nil {
 			panic(e)
 		}
-		target.Close()
+		var t io.Writer = target
+		_, e = io.Copy(t, *file)
+		if e != nil {
+			panic(e)
+		}
+		e = target.Close()
+		if e != nil {
+			panic(e)
+		}
 	}
 	dockerFileGen(ty)
-	err := Tar("./", "docker.tar", false)
+	err = Tar("./", "docker.tar", false)
 	if err != nil {
 		panic(err)
 	}
@@ -116,7 +129,9 @@ func doBuild(reader *io.Reader, simpleBuildInfo *ImageSimpleBuildInfo, ch chan<-
 	defer body.Close()
 	ch <- struct{}{}
 }
-
+func IsNotNil(item interface{}) bool {
+	return item != nil
+}
 func dockerFileGen(ty int) {
 	username := beego.AppConfig.String("buildusername")
 	email := beego.AppConfig.String("buildemail")
@@ -125,34 +140,26 @@ func dockerFileGen(ty int) {
 		panic(err)
 	}
 	i := template.New("Dockerfile")
+	fm := template.FuncMap{"IsNotNil": IsNotNil}
+	i.Funcs(fm)
 	parse, e := i.Parse(`FROM  {{.BaseImage}}
 MAINTAINER  {{.UserName}}  {{.Email}}
 ADD  ./*   {{.Location}}
-EXPOSE  {{.Port}}`)
+EXPOSE  {{.Port}} {{if IsNotNil .Cmd }}
+CMD  {{.Cmd}}{{end}}`)
 	if e != nil {
 		panic(e)
 	}
 	var info DockerfileInfo
 	switch ty {
 	case NGINX:
-		info = DockerfileInfo{
-			BaseImage: "nginx",
-			UserName:  username,
-			Email:     email,
-			Location:  "/usr/share/nginx/html/",
-			Port:      "80",
-		}
+		info = DockerfileInfo{BaseImage: "nginx", UserName: username, Email: email, Location: "/usr/share/nginx/html/", Port: "80",}
 	case JAR:
+		info = DockerfileInfo{BaseImage: "openjdk:8", UserName: username, Email: email, Location: "/root/", Port: "80", Cmd: `["java", "-jar","/root/*.jar"]`,}
 	case TOMCAT:
-		info = DockerfileInfo{
-			BaseImage: "tomcat:jdk8",
-			UserName:  username,
-			Email:     email,
-			Location:  " /usr/local/tomcat/webapps/",
-			Port:      "8080",
-		}
-	case SELF:
+		info = DockerfileInfo{BaseImage: "tomcat:jdk8", UserName: username, Email: email, Location: " /usr/local/tomcat/webapps/", Port: "8080",}
 	default:
+		panic("不支持的打包类型")
 	}
 	err = parse.Execute(dockerfile, info)
 	if err != nil {
